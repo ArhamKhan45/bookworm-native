8b00d848378e76f8c1421c606dedfe6222ad43a8
ArhamKhan45 <karham45@gmail.com>
2025-09-23 10:10:25 +0530

    wip

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,14 +1,15 @@
 import { Text, View } from "react-native";
+import { Image } from "expo-image";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
+      <Image
+        source={{
+          uri: "https://static.wikia.nocookie.net/dragonball/images/b/ba/Goku_anime_profile.png/revision/latest/smart/width/250/height/250?cb=20250723190513",
+        }}
+        style={{ width: 250, height: 250 }}
+      />
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
